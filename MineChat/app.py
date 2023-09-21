a490adbb108627d29b780af22234c1d9472cf219
Soutrik Das <144534392+TheRealSoutrikDas@users.noreply.github.com>
2023-09-21 16:33:51 +0530

    Add files via upload

--- MineChat/app.py
+++ MineChat/app.py
@@ -7,7 +7,7 @@ from chat import get_response
 app = Flask(__name__)
 CORS(app)
 
-@app.get("/")
+@app.get("/chat")
 def index_get():
     return render_template("base.html")
 @app.post("/predict")
@@ -17,6 +17,11 @@ def predict():
     response=get_response(text)
     message={"answer":response}
     return jsonify(message)
-
+@app.route("/")
+def home():
+    return render_template("home.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
 if __name__ == "__main__":
     app.run(debug=True)
